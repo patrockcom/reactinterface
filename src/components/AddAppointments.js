e18858eb2130a811dfb84e10fb8b8a61d5b3c833
patrick.roth <patrick.roth@me.com>
2021-02-21 16:15:09 +0100

    first commit

---
+++ src/components/AddAppointments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AddAppointments extends Component {
+    render() {
+        return <div>Add Appointments</div>;
+    }
+}
+
+export default AddAppointments;
---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myAppointmnents: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+       const apts = result.map(item => {
+         return item;
+      })
+      this.setState({
+        myAppointmnents: apts
+      });
+  });
+}  
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
